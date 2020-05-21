172e1d2bcb237e1ffaf398f7e73a9b7cc0868e45
zengzih <zengzih@163.com>
2020-05-21 20:47:13 +0800

    'img'

--- pages/serviceRegister/index.js
+++ pages/serviceRegister/index.js
@@ -7,11 +7,13 @@ Page({
   data: {
     region: [],
     headUrl: '',
+    currentStep: 3,
+    professionsList: [],
     formData: {
       avatar: '',
       bookName: '',
       sex: '', //  1-男 2-女
-      districtsId: [],
+      districtsId: '',
       address: '',
       remark: '',
       workImages: '',
@@ -20,7 +22,7 @@ Page({
       cardNum: '',
       cardTop: '',
       cardBottom: '',
-      professions: '',
+      professions: ''
     },
   },
 
@@ -46,41 +48,18 @@ Page({
           header: {
             Authorization: token,
           },
-          url: app.location + 'file/upload/image', //仅为示例，非真实的接口地址
+          url: app.location + 'file/upload/image',
           filePath: tempFilePaths[0],
           name: 'file',
           success(res) {
             console.log(res)
             const { data } = JSON.parse(res.data)
             self.setData({
-              headUrl: data.url,
+              'formData.avatar': data.url,
             })
           },
         })
       },
-    })
-  },
-
-  selectFile(files) {
-    const { tempFilePaths } = files
-    let bool = true
-    tempFilePaths.forEach((file) => {
-      const fileName = file.split('.')
-      const fileType = fileName[fileName.length - 1]
-      if (!fileType.test(/PNG|JPG|jpeg/i)) {
-        bool = false
-      }
-    })
-    return bool
-  },
-
-  uplaodFile(file) {
-    return new Promise((resolve, reject) => {
-      // const tempFiles = file.contents
-      // app.store.dispatch('uploadImage', tempFiles).then((res) => {
-      //   console.log(res)
-      // })
-      wx.ch
     })
   },
 
@@ -91,18 +70,141 @@ Page({
     })
   },
 
+  publicUploadImage(url = '') {
+    const token = wx.getStorageSync('token')
+    return new Promise((resolve, reject)=> {
+      wx.chooseImage({
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success(res) {
+          const { tempFilePaths } = res
+          wx.uploadFile({
+            header: {
+              Authorization: token,
+            },
+            url: url || app.location + 'file/upload/image',
+            filePath: tempFilePaths[0],
+            name: 'file',
+            success(res) {
+              const { data } = JSON.parse(res.data)
+              resolve(data)
+            },
+            reject(err) {
+              reject(err)
+            }
+          })
+        },
+      });
+    })
+  },
+
+  handleIdCardImage(event) {
+    const { prop } = event.currentTarget.dataset;
+    const { formData } = this.data;
+    this.publicUploadImage().then(data=> {
+      formData[prop] = data.url;
+      this.setData({formData});
+    })
+  },
+
+  handlePrev() {
+    console.log(111)
+    let { currentStep } = this.data;
+    this.setData({
+      currentStep: currentStep - 1
+    });
+  },
+
+  handleAdd() {
+    const { professionsList } = this.data;
+    professionsList.push({ professionName: '', professionImage: '' })
+    this.setData({ professionsList });
+  },
+
+  handleCertificate(event) {
+    const { value } = event.detail;
+    const { index } = event.currentTarget.dataset;
+    const { professionsList } = this.data;
+    professionsList[index]['professionName'] = value;
+    this.setData({ professionsList });
+  },
+
+  handleUploadCertificate(event) {
+    const { index } = event.currentTarget.dataset;
+    const { professionsList } = this.data;
+    this.publicUploadImage().then(data=> {
+      professionsList[index]['professionImage'] = data.url;
+      this.setData({ professionsList });
+    });
+  },
+
   handleSubmit() {
     // 提交表单
+    const { currentStep } = this.data;
+    if (currentStep == 1) {
+      this.setData({
+        'formData.districtsId': this.data.region.join(',')
+      });
+      const verify = [
+          { label: '头像', prop: 'avatar' },
+          { label: '昵称', prop: 'bookName' },
+          { label: '性别', prop: 'sex' },
+          { label: '省份', prop: 'avatar' },
+          { label: '地址', prop: 'address' }
+      ]
+      if (this.formDataValidate(verify)) {
+        this.setData({currentStep: 2})
+      }
+    }
+
+    if (currentStep == 2) {
+      const verify = [
+        { label: '姓名', prop: 'realName' },
+        { label: '身份证号', prop: 'cardNum' },
+        { label: '身份证正面', prop: 'cardTop' },
+        { label: '身份证反面', prop: 'cardBottom' }
+      ]
+      if (this.formDataValidate(verify)) {
+        this.setData({currentStep: 3})
+      }
+    }
+
+    if (currentStep == 3) {
+
+    }
+  },
+
+  formDataValidate(data) {
+    let validate = true;
+    const { formData } = this.data;
+    for(let index in data){
+      if (!formData[data[index].prop]) {
+        wx.showToast({
+          icon: 'none',
+          title: data[index].label + '没有填写',
+        });
+        validate = false;
+        break
+      }
+    }
+    return validate;
+  },
+
+  handleInputEvent(event) {
+    const { prop } = event.currentTarget.dataset;
+    const { value } = event.target.detail;
+    const { formData } = this.data;
+    formData[prop] = value
+    this.setData({
+      formData
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      selectFile: this.selectFile.bind(this),
-      uplaodFile: this.uplaodFile.bind(this),
-    })
+    this.handleAdd()
   },
 
   /**
